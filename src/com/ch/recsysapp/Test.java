b1a0e7eb6a108721caf5a87d3834283e2d11890b
Zz-m <adjim1234@163.com>
2014-01-21 11:03:04 +0800

    develop on main page.

--- src/com/ch/recsysapp/Test.java
+++ src/com/ch/recsysapp/Test.java
@@ -1,5 +1,9 @@
 package com.ch.recsysapp;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
 public class Test {
 
 	/**
@@ -12,3 +16,20 @@ public class Test {
 	}
 
 }
+//protected void onCreate(Bundle savedInstanceState) {
+//	super.onCreate(savedInstanceState);
+//
+//	setContentView(R.layout.activity_main);
+//	Intent intent = new Intent();
+//	intent.setClass(MainActivity.this, LoginActivity.class);
+//
+//	final Activity thisActivity = this;
+//
+//	boolean flag = false;
+//	if (flag) {
+//		flag = false;
+//		startActivity(intent);
+//
+//	}
+//
+//}
