c230668ad9f1ae52f81ea777547e1272afd39142
Zz-m <adjim1234@163.com>
2014-01-23 16:35:56 +0800

    develop on MainActivity use ListView with SimpleAdapter,it works well.

--- src/com/ch/recsysapp/MainActivity.java
+++ src/com/ch/recsysapp/MainActivity.java
@@ -1,7 +1,12 @@
 package com.ch.recsysapp;
+
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.graphics.BitmapFactory;
 import android.graphics.Matrix;
@@ -16,41 +21,59 @@ import android.view.View.OnClickListener;
 import android.view.animation.Animation;
 import android.view.animation.TranslateAnimation;
 import android.view.ViewGroup;
+import android.widget.AbsListView;
+import android.widget.AbsListView.OnScrollListener;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
-	private ViewPager viewPager;//页卡内容
+	private ViewPager viewPager;// 页卡内容
 	private ImageView imageView;// 动画图片
-	private TextView textView1,textView2,textView3;
+	private TextView textView1, textView2, textView3;
 	private List<View> views;// Tab页面列表
 	private int offset = 0;// 动画图片偏移量
 	private int currIndex = 0;// 当前页卡编号
 	private int bmpW;// 动画图片宽度
-	private View view1,view2,view3;//各个页卡
+	private View view1, view2, view3;// 各个页卡
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		
+
 		Intent intent = new Intent();
 		intent.setClass(MainActivity.this, LoginActivity.class);
 		startActivity(intent);
-		
+
 		setContentView(R.layout.activity_main);
 		InitImageView();
 		InitTextView();
 		InitViewPager();
+
 	}
 
 	private void InitViewPager() {
-		viewPager=(ViewPager) findViewById(R.id.vPager);
-		views=new ArrayList<View>();
-		LayoutInflater inflater=getLayoutInflater();
-		view1=inflater.inflate(R.layout.activity_main_lay1, null);
-		view2=inflater.inflate(R.layout.activity_main_lay2, null);
-		view3=inflater.inflate(R.layout.activity_main_lay3, null);
+		viewPager = (ViewPager) findViewById(R.id.vPager);
+		views = new ArrayList<View>();
+		LayoutInflater inflater = getLayoutInflater();
+		view1 = inflater.inflate(R.layout.activity_main_lay1, null);
+		view2 = inflater.inflate(R.layout.activity_main_lay2, null);
+		view3 = inflater.inflate(R.layout.activity_main_lay3, null);
+		/* vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv */
+
+		ListView lv = (ListView) view1.findViewById(R.id.activity_main_lay1_listview);
+		SimpleAdapter adapter = new SimpleAdapter(this,getData(),R.layout.activity_main_listsyle,
+				new String[]{"title","info","img"},
+				new int[]{R.id.title,R.id.info,R.id.img});
+		lv.setAdapter(adapter);
+		
+
+		/* ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ */
 		views.add(view1);
 		views.add(view2);
 		views.add(view3);
@@ -58,9 +81,45 @@ public class MainActivity extends Activity {
 		viewPager.setCurrentItem(0);
 		viewPager.setOnPageChangeListener(new MyOnPageChangeListener());
 	}
-	 /**
-	  *  初始化头标
-	  */
+
+	/**
+	 * 获取数据，提供给simpleadapter
+	 * @return
+	 */
+	private List<Map<String, Object>> getData() {
+		List<Map<String, Object>> list = new ArrayList<Map<String, Object>>();
+
+		Map<String, Object> map = new HashMap<String, Object>();
+		map.put("title", "G1");
+		map.put("info", "google 1");
+		map.put("img", R.drawable.cat);
+		list.add(map);
+
+		map = new HashMap<String, Object>();
+		map.put("title", "G2");
+		map.put("info", "google 2");
+		map.put("img", R.drawable.cat);
+		list.add(map);
+
+		map = new HashMap<String, Object>();
+		map.put("title", "G3");
+		map.put("info", "google 3");
+		map.put("img", R.drawable.cat);
+		list.add(map);
+		for(int i = 0; i < 7; i++) {
+			map = new HashMap<String, Object>();
+			map.put("title", "G3");
+			map.put("info", "google 3");
+			map.put("img", R.drawable.cat);
+			list.add(map);
+		}
+		
+		return list;
+	}
+
+	/**
+	 * 初始化头标
+	 */
 
 	private void InitTextView() {
 		textView1 = (TextView) findViewById(R.id.text1);
@@ -73,12 +132,13 @@ public class MainActivity extends Activity {
 	}
 
 	/**
-	 2      * 初始化动画，这个就是页卡滑动时，下面的横线也滑动的效果，在这里需要计算一些数据
-	 3 */
+	 * 2 * 初始化动画，这个就是页卡滑动时，下面的横线也滑动的效果，在这里需要计算一些数据 3
+	 */
 
 	private void InitImageView() {
-		imageView= (ImageView) findViewById(R.id.cursor);
-		bmpW = BitmapFactory.decodeResource(getResources(), R.drawable.cursor_main).getWidth();// 获取图片宽度
+		imageView = (ImageView) findViewById(R.id.cursor);
+		bmpW = BitmapFactory.decodeResource(getResources(),
+				R.drawable.cursor_main).getWidth();// 获取图片宽度
 		DisplayMetrics dm = new DisplayMetrics();
 		getWindowManager().getDefaultDisplay().getMetrics(dm);
 		int screenW = dm.widthPixels;// 获取分辨率宽度
@@ -87,100 +147,93 @@ public class MainActivity extends Activity {
 		matrix.postTranslate(offset, 0);
 		imageView.setImageMatrix(matrix);// 设置动画初始位置
 	}
-//<img src="http://img.my.csdn.net/uploads/201211/10/1352554452_1685.jpg" alt="">
-	/** 
-	 *     
-	 * 头标点击监听 3 */
-	private class MyOnClickListener implements OnClickListener{
-        private int index=0;
-        public MyOnClickListener(int i){
-        	index=i;
-        }
-		public void onClick(View v) {
-			viewPager.setCurrentItem(index);			
+
+	// <img src="http://img.my.csdn.net/uploads/201211/10/1352554452_1685.jpg"
+	// alt="">
+	/**
+	 * 
+	 * 头标点击监听 3
+	 */
+	private class MyOnClickListener implements OnClickListener {
+		private int index = 0;
+
+		public MyOnClickListener(int i) {
+			index = i;
 		}
-		
+
+		public void onClick(View v) {
+			viewPager.setCurrentItem(index);
+		}
+
 	}
-	
-	public class MyViewPagerAdapter extends PagerAdapter{
+
+	public class MyViewPagerAdapter extends PagerAdapter {
 		private List<View> mListViews;
-		
+
 		public MyViewPagerAdapter(List<View> mListViews) {
 			this.mListViews = mListViews;
 		}
 
 		@Override
-		public void destroyItem(ViewGroup container, int position, Object object) 	{	
+		public void destroyItem(ViewGroup container, int position, Object object) {
 			container.removeView(mListViews.get(position));
 		}
 
-
 		@Override
-		public Object instantiateItem(ViewGroup container, int position) {			
-			 container.addView(mListViews.get(position), 0);
-			 return mListViews.get(position);
+		public Object instantiateItem(ViewGroup container, int position) {
+			container.addView(mListViews.get(position), 0);
+			return mListViews.get(position);
 		}
 
 		@Override
-		public int getCount() {			
-			return  mListViews.size();
+		public int getCount() {
+			return mListViews.size();
 		}
-		
+
 		@Override
-		public boolean isViewFromObject(View arg0, Object arg1) {			
-			return arg0==arg1;
+		public boolean isViewFromObject(View arg0, Object arg1) {
+			return arg0 == arg1;
 		}
 	}
 
-    public class MyOnPageChangeListener implements OnPageChangeListener{
+	public class MyOnPageChangeListener implements OnPageChangeListener {
 
-    	int one = offset * 2 + bmpW;// 页卡1 -> 页卡2 偏移量
+		int one = offset * 2 + bmpW;// 页卡1 -> 页卡2 偏移量
 		int two = one * 2;// 页卡1 -> 页卡3 偏移量
+
 		public void onPageScrollStateChanged(int arg0) {
-			
-			
+
 		}
 
 		public void onPageScrolled(int arg0, float arg1, int arg2) {
-			
-			
+
 		}
 
 		public void onPageSelected(int arg0) {
-			/*两种方法，这个是一种，下面还有一种，显然这个比较麻烦
-			Animation animation = null;
-			switch (arg0) {
-			case 0:
-				if (currIndex == 1) {
-					animation = new TranslateAnimation(one, 0, 0, 0);
-				} else if (currIndex == 2) {
-					animation = new TranslateAnimation(two, 0, 0, 0);
-				}
-				break;
-			case 1:
-				if (currIndex == 0) {
-					animation = new TranslateAnimation(offset, one, 0, 0);
-				} else if (currIndex == 2) {
-					animation = new TranslateAnimation(two, one, 0, 0);
-				}
-				break;
-			case 2:
-				if (currIndex == 0) {
-					animation = new TranslateAnimation(offset, two, 0, 0);
-				} else if (currIndex == 1) {
-					animation = new TranslateAnimation(one, two, 0, 0);
-				}
-				break;
-				
-			}
-			*/
-			Animation animation = new TranslateAnimation(one*currIndex, one*arg0, 0, 0);//显然这个比较简洁，只有一行代码。
+			/*
+			 * 两种方法，这个是一种，下面还有一种，显然这个比较麻烦 Animation animation = null; switch
+			 * (arg0) { case 0: if (currIndex == 1) { animation = new
+			 * TranslateAnimation(one, 0, 0, 0); } else if (currIndex == 2) {
+			 * animation = new TranslateAnimation(two, 0, 0, 0); } break; case
+			 * 1: if (currIndex == 0) { animation = new
+			 * TranslateAnimation(offset, one, 0, 0); } else if (currIndex == 2)
+			 * { animation = new TranslateAnimation(two, one, 0, 0); } break;
+			 * case 2: if (currIndex == 0) { animation = new
+			 * TranslateAnimation(offset, two, 0, 0); } else if (currIndex == 1)
+			 * { animation = new TranslateAnimation(one, two, 0, 0); } break;
+			 * 
+			 * }
+			 */
+			Animation animation = new TranslateAnimation(one * currIndex, one
+					* arg0, 0, 0);// 显然这个比较简洁，只有一行代码。
 			currIndex = arg0;
 			animation.setFillAfter(true);// True:图片停在动画结束位置
 			animation.setDuration(300);
 			imageView.startAnimation(animation);
-			Toast.makeText(MainActivity.this, "您选择了"+ viewPager.getCurrentItem()+"页卡", Toast.LENGTH_SHORT).show();
+			Toast.makeText(MainActivity.this,
+					"您选择了" + viewPager.getCurrentItem() + "页卡",
+					Toast.LENGTH_SHORT).show();
 		}
-    	
-    }
+		
+	}
 }
