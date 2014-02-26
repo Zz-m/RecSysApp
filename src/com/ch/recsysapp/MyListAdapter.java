9e6a8708b311f7e9481c52e2c1659bb2dec204c5
Zz-m <adjim1234@163.com>
2014-02-26 17:46:29 +0800

    class reconstruct

--- src/com/ch/recsysapp/MyListAdapter.java
+++ src/com/ch/recsysapp/MyListAdapter.java
@@ -1,6 +1,5 @@
 package com.ch.recsysapp;
 
-import com.ch.recsysapp.module.Item;
 import com.ch.recsysapp.module.ItemList;
 
 import android.content.Context;
@@ -11,44 +10,42 @@ import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+/**
+ * 
+ * @author adj 
+ * listView adapter
+ * 
+ */
 public class MyListAdapter extends BaseAdapter {
 	private Context context;
 	private ItemList itemList;
-	private View view;
 	private LayoutInflater mInflater;
-	
 
 	public MyListAdapter(Context context, ItemList itemList) {
 		this.context = context;
 		this.itemList = itemList;
-		mInflater = LayoutInflater.from(context);
-		view = mInflater.inflate(R.layout.activity_main_lay1_itemsyle, null);
 	}
 
+	/**
+	 * 返回每个item样式
+	 */
 	@Override
 	public View getView(int position, View convertView, ViewGroup parent) {
-//		Item item = itemList.get(position);
-//		ImageView imageView = (ImageView) itemView
-//				.findViewById(R.id.activity_main_img1);
-//		System.out.println("asdasdasd输出错误qian");
-//		if (item.getBitmap() != null && imageView != null) {
-//			System.out.println("bitmap y已经不为空");
-//			imageView.setImageBitmap(item.getBitmap());
-//		}
-//		return itemView;
-		if(convertView == null)
-        {
-            convertView = mInflater.inflate(R.layout.activity_main_lay1_itemsyle, null);
-        }
-        
-        ImageView img = (ImageView)convertView.findViewById(R.id.activity_main_img1) ;
-        TextView title = (TextView)convertView.findViewById(R.id.activity_main_title1);
-        TextView summary = (TextView)convertView.findViewById(R.id.activity_main_info1);
-        img.setImageBitmap(itemList.get(position).getBitmap());
-        title.setText(itemList.get(position).getName());
-        summary.setText(itemList.get(position).getSummary());
-        
-        return convertView;
+		if (convertView == null) {
+			mInflater = LayoutInflater.from(context);
+			convertView = mInflater.inflate(
+					R.layout.activity_main_lay1_itemsyle, null);
+		}
+		ImageView img = (ImageView) convertView
+				.findViewById(R.id.activity_main_img1);
+		TextView title = (TextView) convertView
+				.findViewById(R.id.activity_main_title1);
+		TextView summary = (TextView) convertView
+				.findViewById(R.id.activity_main_info1);
+		img.setImageBitmap(itemList.get(position).getBitmap());
+		title.setText(itemList.get(position).getName());
+		summary.setText(itemList.get(position).getSummary());
+		return convertView;
 	}
 
 	@Override
