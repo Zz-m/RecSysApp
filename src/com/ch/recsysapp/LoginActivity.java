c62cf00d8d75a41212097d4d8d640aa6bf746de3
Zz-m <adjim1234@163.com>
2014-03-06 17:21:35 +0800

    work on login

--- src/com/ch/recsysapp/LoginActivity.java
+++ src/com/ch/recsysapp/LoginActivity.java
@@ -6,6 +6,12 @@ import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.Intent;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.method.LinkMovementMethod;
+import android.text.style.StyleSpan;
+import android.text.style.URLSpan;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.Window;
@@ -21,9 +27,17 @@ import android.widget.TextView;
 
 public class LoginActivity extends Activity {
 
+	TextView myTextView;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		init();
+	}
+	/**
+	 * 初始化
+	 */
+	private void init() {
+		String s = "没有账号？注册";
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.activity_login);
 
@@ -59,6 +73,22 @@ public class LoginActivity extends Activity {
 				startActivity(intent);
 			}
 		});
+		myTextView = (TextView) this.findViewById(R.id.register_link2); 
+		//创建一个 SpannableString对象  
+        SpannableString sp = new SpannableString(s);  
+        //设置超链接  
+        sp.setSpan(new URLSpan("http://www.google.com"), s.length()-2, s.length(),  
+                Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);  
+//        //设置高亮样式一  
+//        sp.setSpan(new BackgroundColorSpan(Color.RED), 17 ,19,Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);  
+//        //设置高亮样式二  
+//        sp.setSpan(new ForegroundColorSpan(888),0, s.length()-3,Spannable.SPAN_EXCLUSIVE_INCLUSIVE);    
+//        //设置斜体  
+        sp.setSpan(new StyleSpan(android.graphics.Typeface.BOLD_ITALIC), 0, s.length()-2, Spannable.SPAN_EXCLUSIVE_INCLUSIVE);  
+        //SpannableString对象设置给TextView  
+        myTextView.setText(sp);  
+        //设置TextView可点击  
+        myTextView.setMovementMethod(LinkMovementMethod.getInstance());  
 	}
 
 }
